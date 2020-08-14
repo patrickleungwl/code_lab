0f3850b64cb219d1dd33c171dc56b3b3f5be21f2
Patrick Leung <patrickleungwl@gmail.com>
2020-08-14 15:43:05 -0400

    Moved all code to ModernCppFeatures

---
+++ cpp_lab/ModernCppFeatures/ModernCppFeatures/ModernCppFeatures.cpp
@@ -0,0 +1,61 @@
+// ModernCppFeatures.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "FeatureTests.h"
+
+int main()
+{
+	FeatureTests tester(1, 2);
+
+	// Explicit keyword with constructor
+	tester.Test_ExplicitConstructor_ExpectOnlyExactParameterMatch();
+
+	// Smart pointers
+	tester.Test_VectorOfRawPointers_ExpectMemoryLeak();
+	tester.Test_VectorOfSharedPointers_ExpectNoMemoryLeak();
+	tester.Test_VectorOfUniquePointers_ExpectNoMemoryLeak();
+	tester.Test_SetObject_ExpectAssignmentOperatorUsed();
+	tester.Test_ContainerOfSmartPointers_ExpectSimpleObjectConstruction();
+
+	// Move semantics
+	tester.Test_MoveOperator_ExpectMoveOperatorUsed();
+	tester.Test_MoveUniquePtr_ExpectOriginalPtrBecomesNull();
+	tester.Test_ContainerPushBackWithNoMemoryPreallocation_ExpectCopyConstructorUsed();
+	tester.Test_ContainerMovePushBackWithNoMemoryPreallocation_ExpectMoveConstructorUsed();
+	tester.Test_ContainerPushBackWithMemoryPreallocation_ExpectCopyConstructorUsed();
+	tester.Test_ContainerMovePushBackWithMemoryPreallocation_ExpectMoveConstructorUsed();
+
+	// Lambdas
+	tester.Test_VectorForEachAlgorithmWithNamedLambda_ExpectIterationToWork();
+	tester.Test_MapForEachAlgorithmWithNamedLambda_ExpectIterationToWork();
+	tester.Test_VectorForIfAlgorithmWithAnonLambda_ExpectToFindItem();
+
+	tester.Test_MakeHeapAlgorithm_ExpectMaxNodeAtRoot();
+
+	/*
+	tester.TestAlgoMakeHeapOnVector();
+	tester.TestAlgoMakeHeapOnVectorOfInts();
+	tester.TestAlgoMakeHeapOnVectorOfWidgets();
+	tester.TestNewThreadWithFunction();
+	tester.TestNewThreadWithLambdaFunction();
+	tester.TestForEachCopies();
+	tester.TestMoveConstructor();
+	tester.TestGenericFunction();
+	tester.TestSetInsertAndEmplace();
+	*/
+
+	int tmp = 10;
+	int tmp2 = 40;
+	//const int const *p = &tmp;
+
+	//int * const pi = &tmp;
+	//pi = &tmp2;
+
+	// Let user check memory usage before exiting
+	//int i;
+	//cin >> i;
+
+	return 0;
+}
+
---
+++ cpp_lab/ModernCppFeatures/ModernCppFeatures/Widget.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include "Widget.h"
+
+using namespace std;
+
+Widget::Widget(int i) : _id(i)
+{
+	++widgetCount;
+	pmemory = new int[widget_size];
+	memset(pmemory, 88, sizeof(int)*widget_size);
+}
+
+Widget::~Widget()
+{
+	--widgetCount;
+	delete pmemory;
+}
+
+/*
+Widget::Widget(Widget& other)
+{
+	++copyConstructorCount;
+	cout << "Widget copy ctor" << endl;
+	_id = other._id;
+	pmemory = new int[widget_size];
+	memcpy(pmemory, other.pmemory, sizeof(int) * widget_size);
+}
+*/
+
+
+Widget::Widget(const Widget &other)
+{
+	++widgetCount;
+	++copyConstructorCount;
+	//cout << "Widget const copy ctor" << endl;
+	_id = other._id;
+	pmemory = new int[widget_size];
+	memcpy(pmemory, other.pmemory, sizeof(int)*widget_size);
+}
+
+
+Widget &Widget::operator=(const Widget &other)
+{
+	++assignmentCount;
+	if (this == &other)
+		return *this;
+
+	_id = other._id;
+	pmemory = new int[widget_size];
+	memcpy(pmemory, other.pmemory, sizeof(int)*widget_size);
+	return *this;
+}
+
+
+Widget::Widget(Widget &&other) noexcept :
+	_id(other._id),
+	pmemory(other.pmemory) 
+{
+	++widgetCount;
+	++moveConstructorCount;
+	//cout << "Widget move ctor" << endl;
+	other.pmemory = nullptr;
+}
+
+
+Widget& Widget::operator=(Widget &&other) noexcept
+{
+	cout << "Widget move optr" << endl;
+	if (this == &other)
+		return *this;
+
+	_id = other._id;
+	pmemory = other.pmemory;
+	other.pmemory = nullptr;
+	return *this;
+}
+
+
+bool Widget::operator==(const Widget &other) const
+{
+	bool result = false;
+	if (this->_id == other._id)
+		result = true;
+	return result;
+}
+
+
+bool Widget::operator<(const Widget &other) const
+{
+	bool result = false;
+	if (this->_id > other._id)
+		result = true;
+	return result;
+}
+
+
+int Widget::getId() const
+{
+	return _id;
+}
+
+
+int Widget::getContents(int memId) const
+{
+	return pmemory[memId];
+}
